fb4853436c90b50446f4acda316b560887873b9f
Federico Lasa <felasa+github@gmail.com>
2026-01-20 16:56:41 -0600

    it works

--- src/dev/venation.clj
+++ src/dev/venation.clj
@@ -1,13 +1,16 @@
 (ns dev.venation
   (:require [quil.core :as q]
-            [clojure.math :as math]))
+            [clojure.math :as math]
+            [quil.middleware :as m]
+            [util.color :as color]))
 
 (defn sq-dist
   "Square distance"
   [p1 p2]
   (->> (map - p1 p2) 
        (map #(* % %))
-       (reduce +)))
+       (reduce +)
+       (math/sqrt)))
 
 (defn v-dif 
   "Vector difference"
@@ -22,16 +25,14 @@
 (defn normalize 
   "Make univ vector"
   [p]
-  (let [norm (math/sqrt (sq-dist p [0 0]))]
+  (let [norm (sq-dist p [0 0])]
     (mapv / p (repeat 2 norm))))
   
 (sq-dist (normalize [1 1]) [0 0])
 (math/sqrt (sq-dist [(/ 2) (/ 2)] [0 0]))
-(def ex-veins {[400 400] nil
-               [350 400] [400 400]
-               [400 450] [400 400]
-               [350 450] [350 400]
-               [450 450] [400 450]})
+(def ex-veins {[400 700] nil
+               [400 690] [400 700]
+               [400 680] [400 690]})
 
 (def ex-auxins
   (->> (fn [] (vector (rand-int 800) (rand-int 800)))
@@ -111,6 +112,8 @@
         auxins (into auxins 
                      (->> (fn [] (vector (rand-int 800) (rand-int 800)))
                           (repeatedly)
+                          (remove #(or (> (sq-dist % [800 400]) 600)
+                                      (> (sq-dist % [0 400])   500)))
                           (take spawn-rate)))
         distances 
         (->> (auxin-distances auxins veins)
@@ -119,44 +122,99 @@
              (apply hash-map))
         filtered (keys distances)
         assigned (update-vals distances  min-by-val)
-        directions (update-vals (->> (group-by #(assigned %) (keys assigned))
-                                     (map (fn [kv] (hash-map (key kv) (mapv (fn [p] (v-dif (key kv) p)) (val kv))))) 
-                                     (reduce conj))
-                                (fn [coll] (mapv #(* segment-len %) (normalize (reduce v-sum coll)))))
+        directions (update-vals
+                     (->> (group-by #(assigned %) (keys assigned))
+                          (map (fn [kv] (hash-map (key kv) (mapv (fn [p] (v-dif p (key kv))) (val kv))))) 
+                          (reduce conj))
+                     (fn [coll] (mapv #(* segment-len %) (normalize (reduce v-sum coll)))))
         nv (->> directions 
-                (map (fn [kv] {(mapv + (key kv) (val kv)) (key kv)}))
+                (map (fn [kv] {(mapv long (mapv + (key kv) (val kv))) (key kv)}))
                 (into veins))
         return {:auxins filtered :veins nv}]
     ;filtered))
     ;distances))
     ;assigned
     ;directions))
-    ;nv
+    ;nv))
     return))
 
-(comment  (step-grow 15000 50 20 {:auxins ex-auxins :veins ex-veins}))
+(comment  (step-grow 15000 150 20 {:auxins ex-auxins :veins ex-veins}))
 
-(def parametrized (partial step-grow 150000 100 25))
+(def parametrized (partial step-grow 25 5 5))
 (parametrized (parametrized {:auxins [] :veins ex-veins}))
 
-(defn gen-veins [iters]
+(defn gen-veins [iters state]
   (loop [iter 1
-         state {:auxins [] :veins {[400 400] nil}}]
+         state state]  
     (if (< iter iters)
       (recur (inc iter) (parametrized state))
       state)))
 
 
-(def algo (gen-veins 100))
-(first (:veins algo))
+
+(parametrized {:auxins ex-auxins :veins ex-veins})
+
+(def sanzo (read-string (slurp "resources/sanzo-colors.edn")))
+(def palette (shuffle (mapv color/hex-to-rgb (:hex (rand-nth (:combos sanzo))))))
+
+(defn setup-loop []
+  (q/frame-rate 12)
+  ;(q/background 214,180,62)
+  (apply q/background (palette 0)) 
+  {:auxins [[400 400]] :veins {[400 750] nil}
+   :iter 1})
+
+
+(defn looped-draw
+  [state]
+  ;(q/frame-rate 12)
+  ;(q/background 214,180,62)
+  ;(apply q/background (palette 0)) 
+  (let [auxins (:auxins state)
+        veins (:veins state)]
+    (q/no-stroke)
+    (q/fill 255 0 0)
+    (q/text-size 20)
+    (q/text (str (q/frame-count)) 10 10)
+    (doseq [p auxins])
+      ;(q/ellipse (p 0) (p 1) 10 10))
+    (doseq [e veins]
+      (let [p1 (key e), p2 (val e)]
+        (when p2 
+           (apply q/stroke (palette 1)) 
+           ;(q/stroke 107,113,64)
+           (q/stroke-weight 4) 
+           (q/line p1 p2))
+           ;(q/stroke 0 0 0))))))
+           ;(q/stroke-weight 1)
+           ;(q/line p1 p2))))))
+        (q/no-stroke)
+        (q/ellipse (p1 0) (p1 1) 2 2)))))
+     
+(defn update-loop [R-exclusion segment-len spawn-rate]
+  (fn [state]
+    (let [ns ((partial step-grow R-exclusion segment-len spawn-rate) state)
+          ni (inc (:iter state))]
+      (assoc ns :iter ni))))
+
+(looped-draw {:auxins ex-auxins :veins ex-veins})
+        
+(def algo (gen-veins 100 {}))
+
 (declare skveins)
 (q/defsketch skveins
-  :setup (fn [] (q/no-loop))
+  :middleware [m/fun-mode]
+  :setup setup-loop
+  :update (update-loop 25 5 3)
   :size [800 800]
-  :draw (fn [] 
-          (let [veins (remove #(nil? (val %)) (:veins (gen-veins 100)))]
-            (doseq [e veins]
-              (q/ellipse ((key e) 0) ((key e) 1) 10 10))))) 
-
-(doseq [v (:veins algo)]
-  (println (v 0) (v 1)))
+  :draw looped-draw
+  :drawa (fn [] 
+           (let [veins (remove #(nil? (val %))
+                               (:veins (gen-veins 100 {:auxins ex-auxins :veins ex-veins})))]
+             (doseq [e veins]
+               (q/line (key e) (val e))
+               (q/ellipse ((key e) 0) ((key e) 1) 10 10))))) 
+  
+(comment 
+  (quil.applet/with-applet dev.venation/skveins))
+   
